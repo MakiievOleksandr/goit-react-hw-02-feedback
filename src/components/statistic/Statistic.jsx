6a362e2023ecc32ae6ba9d9d20d96811f19db997
MakiievOleksandr <alsermak@gmail.com>
2023-01-18 15:30:04 +0200

    add some styles

--- src/components/statistic/Statistic.jsx
+++ src/components/statistic/Statistic.jsx
@@ -2,19 +2,30 @@ import PropTypes from 'prop-types';
 
 import css from './statistic.module.css';
 
-function Statistic({ good, neutral, bad, total, positivePercentage }) {
-  return (
-    <div className={css.statsWraper}>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p className={positivePercentage < 50 ? css.negative : css.positive}>
-        Positive feedback:{' '}
-        {positivePercentage === 0 ? 0 : `${positivePercentage}%`}
+function Statistic({ ...props }) {
+  const items = Object.keys(props);
+  const values = Object.values(props);
+
+  const elem = items.map((item, index) => {
+    return (
+      <p key={index}>
+        {item}: {values[index]}
       </p>
-    </div>
+    );
+  });
+  const positive = (
+    <p
+      key={elem.length - 1}
+      className={props.positivePercentage < 50 ? css.negative : css.positive}
+    >
+      Positive feedback:{' '}
+      {props.positivePercentage === 0 ? 0 : `${props.positivePercentage}%`}
+    </p>
   );
+
+  elem.splice(elem.length - 1, 1, positive);
+
+  return <div className={css.statsWraper}>{elem}</div>;
 }
 
 export default Statistic;
